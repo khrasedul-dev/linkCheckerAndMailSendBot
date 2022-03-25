0bd19151010d883b892eabafce25c510a5ec48d8
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-03-25 16:49:36 +0600

    Update app.js

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ bot.command('check',async (ctx)=>{
                             })
                             
                             const page = await browser.newPage()
+                            page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36')
                             
                             page.setDefaultNavigationTimeout(100000)
                         
